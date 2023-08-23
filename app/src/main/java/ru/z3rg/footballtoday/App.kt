6b7a14f3eb05e7b8ec2cdc379a04aa820f877bf0
Denisov Nikita <sollwarmorgrind@gmail.com>
2023-08-23 03:41:42 +0300

    Подключение OneSignal

--- app/src/main/java/ru/z3rg/footballtoday/App.kt
+++ app/src/main/java/ru/z3rg/footballtoday/App.kt
@@ -1,7 +1,20 @@
 package ru.z3rg.footballtoday
 
 import android.app.Application
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
 
+const val ONESIGNAL_APP_ID = "84b704ce-4320-4883-9178-d34dc9cfa62e"
+
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID)
+        OneSignal.User.pushSubscription
+
+    }
+}
